c400db7837cd80265335ae7893b5bf66038e23bd
digovc <rodrigo.conspena@gmail.com>
2024-06-24 16:56:38 -0300

    Fix runtime baseurl.

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  assetsInclude: ['./src/prompts/*.md']
+  assetsInclude: ['./src/prompts/*.md'],
+  base: process.env.NODE_ENV === 'production'
+    ? '/artifacts-ai/'
+    : '/',
 })
