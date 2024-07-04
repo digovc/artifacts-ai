18aa65482763392fc26c60bda04ba41bc94b3664
digovc <rodrigo.conspena@gmail.com>
2024-07-04 20:54:20 -0300

    Allow close modal with esc.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,10 @@
 <template>
   <Teleport to="body">
     <div class="absolute inset-0 bg-black z-10 bg-opacity-50 flex justify-center items-center"
-         @click="$emit('onClose')">
+         @click="closeModal"
+         @keydown.esc="closeModal"
+         tabindex="0"
+         ref="modalContainer">
       <Card @click.stop class="space-y-4">
         <div class="font-bold">
           <slot name="title"/>
@@ -11,7 +14,7 @@
         </div>
         <div class="text-right space-x-2">
           <slot name="commands"/>
-          <Button @click="$emit('onClose')">
+          <Button @click="closeModal">
             Cancel
           </Button>
         </div>
@@ -19,9 +22,31 @@
     </div>
   </Teleport>
 </template>
+
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
 import Card from "@/components/Card.vue";
 import Button from "@/components/Button.vue";
 
-defineEmits(["onClose"]);
+const emit = defineEmits(["onClose"]);
+const modalContainer = ref(null);
+
+const closeModal = () => {
+  emit('onClose');
+};
+
+const handleKeyDown = (event) => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+};
+
+onMounted(() => {
+  document.addEventListener('keydown', handleKeyDown);
+  modalContainer.value.focus();
+});
+
+onUnmounted(() => {
+  document.removeEventListener('keydown', handleKeyDown);
+});
 </script>
