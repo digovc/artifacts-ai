8897535d944958b418657c7ef4ddbed3ff700d7c
digovc <rodrigo.conspena@gmail.com>
2024-07-06 15:06:12 -0300

    Add icon to chip component.

--- src/components/Chip.vue
+++ src/components/Chip.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex space-x-4 rounded pl-4 pr-3 py-1 cursor-pointer items-center hover:bg-gray-200 group border"
        :class="{ 'bg-gray-200': isSelected }">
+    <div v-if="icon" class="mr-2">
+      <FontAwesomeIcon :icon="icon" class="text-sm"/>
+    </div>
     <div class="whitespace-nowrap text-sm" @click="$emit('onClick')">
       <slot/>
     </div>
@@ -9,6 +12,7 @@
     </div>
   </div>
 </template>
+
 <script setup>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
@@ -17,5 +21,9 @@ defineEmits(["onClick", "onDeleteClick"]);
 
 defineProps({
   isSelected: Boolean,
-})
+  icon: {
+    type: Object,
+    default: null,
+  },
+});
 </script>
